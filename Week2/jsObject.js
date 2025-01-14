b0c642f8f3b853269f3661a8f8c6f19c5c8cb87e
Anit <110488522+kand5950@users.noreply.github.com>
2025-01-14 12:02:44 -0500

    object practice properties

---
+++ Week2/jsObject.js
@@ -0,0 +1,10 @@
+// javascript object syntax example
+
+let student = {
+    studentID: 12345,
+    studentName: "John",
+    studentPhone: "123-456-7890",
+    studentEmail: "john@doe.com"
+};
+
+console.log(`Name : ${student.studentName}`);
